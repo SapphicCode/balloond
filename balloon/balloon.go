c9c41dbe72bafefcc743583c30f0c0367b6a441d
Cassandra <cassandra@beelen.one>
2019-07-20 20:27:25 +0000

    Add caching in preparation for #1 (#2)

--- balloon/balloon.go
+++ balloon/balloon.go
@@ -7,13 +7,16 @@ import (
 	"time"
 
 	"github.com/digitalocean/go-libvirt"
+	"github.com/patrickmn/go-cache"
 	"github.com/rs/zerolog"
 )
 
 // Balloon describes a hypervisor memory balloon interface.
 type Balloon struct {
 	libvirt *libvirt.Libvirt
-	Logger  zerolog.Logger
+	cache   *cache.Cache
+
+	Logger zerolog.Logger // Logger is the zerolog Logger. New creates a default, but feel free to override it.
 
 	Interval time.Duration // Interval is the interval at which the daemon should refresh all domain balloons.
 	DryRun   bool          // DryRun effectively pretends running actions like DomainSetMemory, but doesn't.
@@ -25,8 +28,11 @@ type Balloon struct {
 // New creates a Balloon from a net.Conn and populates its settings with default values.
 func New(conn net.Conn) Balloon {
 	l := libvirt.New(conn)
+	c := cache.New(1*time.Minute, 1*time.Second)
 	return Balloon{
 		l,
+		c,
+
 		zerolog.New(os.Stdout),
 
 		10 * time.Second,
